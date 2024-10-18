537fa4756c9fe3beede2286c92b250190a61c5cc
mhussain <mhussain@stellatechnology.com>
2024-10-18 12:41:33 +0500

    Git Hub Action

---
+++ github-cation/src/main/java/com/devops/github/action/GithubCationApplication.java
@@ -0,0 +1,13 @@
+package com.devops.github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCationApplication.class, args);
+	}
+
+}
