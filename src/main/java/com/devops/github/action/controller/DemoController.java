43562487bd6c21f762d5d9ada974f1ce7324b199
mhussain <mhussain@stellatechnology.com>
2024-10-24 12:02:20 +0500

    Add New End Point

--- src/main/java/com/devops/github/action/controller/DemoController.java
+++ src/main/java/com/devops/github/action/controller/DemoController.java
@@ -11,4 +11,17 @@ public class DemoController {
     public String helloWord() {
         return "<h1>Hello World</h1>";
     }
+
+    @GetMapping("/hello2")
+    public String secondWord() {
+        StringBuffer sb = new StringBuffer();
+
+        if (sb != null) {
+            sb.append("<h1>Hello World</h1>");
+        }
+
+        return sb.toString();
+
+    }
+
 }
